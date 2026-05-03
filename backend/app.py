2ed6d9ac7923af5e1aed075da24b6203d77a7fc6
dumkatex <dumka.tex@gmail.com>
2026-05-03 08:33:16 +0300

    init clean

---
+++ backend/app.py
@@ -0,0 +1,86 @@
+from fastapi import FastAPI
+import psycopg2
+import os
+
+
+app = FastAPI()
+
+DB_CONFIG = {
+    "host": "postgres",
+    "database": os.getenv("POSTGRES_DB"),
+    "user": os.getenv("POSTGRES_USER"),
+    "password": os.getenv("POSTGRES_PASSWORD"),
+}
+
+
+
+
+
+@app.get("/")
+def read_root():
+    return {"message": "Backend is working"}
+
+
+@app.get("/db")
+def check_db():
+    conn = psycopg2.connect(**DB_CONFIG)
+    cur = conn.cursor()
+
+    cur.execute("SELECT version();")
+    result = cur.fetchone()
+
+    cur.close()
+    conn.close()
+
+    return {"postgres_version": result[0]}
+
+
+@app.get("/init")
+def init_db():
+    conn = psycopg2.connect(**DB_CONFIG)
+    cur = conn.cursor()
+
+    cur.execute("""
+        CREATE TABLE IF NOT EXISTS users (
+            id SERIAL PRIMARY KEY,
+            name TEXT
+        );
+    """)
+
+    conn.commit()
+    cur.close()
+    conn.close()
+
+    return {"status": "table created"}
+
+
+@app.get("/add/{name}")
+def add_user(name: str):
+    conn = psycopg2.connect(**DB_CONFIG)
+    cur = conn.cursor()
+
+    cur.execute(
+        "INSERT INTO users (name) VALUES (%s) RETURNING id;",
+        (name,)
+    )
+    user_id = cur.fetchone()[0]
+
+    conn.commit()
+    cur.close()
+    conn.close()
+
+    return {"id": user_id, "name": name}
+
+
+@app.get("/users")
+def get_users():
+    conn = psycopg2.connect(**DB_CONFIG)
+    cur = conn.cursor()
+
+    cur.execute("SELECT id, name FROM users;")
+    rows = cur.fetchall()
+
+    cur.close()
+    conn.close()
+
+    return [{"id": r[0], "name": r[1]} for r in rows]
